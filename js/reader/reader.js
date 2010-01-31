8768399cbf7477b256551e6439e3ec801d518e1b
tim <tim3d.junk@gmail.com>
2010-01-31 21:20:12 +1100

    rename this to self

--- js/reader/reader.js
+++ js/reader/reader.js
@@ -1,8 +1,9 @@
 
 function GoogleReader() {
-	this.token = null;
+	var self = this;
+	self.token = null;
 
-	this.login = function(user, pass, cb) {
+	self.login = function(user, pass, cb) {
 		data = {
 			service: 'reader',
 			Email: user,
@@ -27,39 +28,39 @@ function GoogleReader() {
 		});
 	};
 
-	this.GET = function(url, data, cb, err) {
-		data['SID'] = this.sid;
+	self.GET = function(url, data, cb, err) {
+		data['SID'] = self.sid;
 		GET(url, data, cb, err);
 	};
 
-	this.POST = function(url, data, cb, err) {
-		data['SID'] = this.sid;
+	self.POST = function(url, data, cb, err) {
+		data['SID'] = self.sid;
 		POST(url, data, cb, err);
 	};
 
-	this.get_token = function(force, cb) {
-		if(force || !this.token) {
+	self.get_token = function(force, cb) {
+		if(force || !self.token) {
 			var feedurl = GoogleReaderConst.URI_PREFIXE_API + GoogleReaderConst.API_TOKEN;
-			this.GET(feedurl, {client: GoogleReaderConst.AGENT}, cb);
+			self.GET(feedurl, {client: GoogleReaderConst.AGENT}, cb);
 		} else {
-			cb(this.token);
+			cb(self.token);
 		}
 	};
 
 	// low-level:
 
-	this.get_api_list = function(url, data, cb) {
+	self.get_api_list = function(url, data, cb) {
 		data['output'] = GoogleReaderConst.OUTPUT_JSON;
 		data['client'] = GoogleReaderConst.AGENT;
-		this.GET(url, data, function(obj){ cb(JSON.parse(obj)); });
+		self.GET(url, data, function(obj){ cb(JSON.parse(obj)); });
 	};
 
-	this.get_timestamp = function() {
+	self.get_timestamp = function() {
 		return new Date().getTime();
 	};
 
 
-	this._translate_args = function(dictionary, googleargs, kwargs) {
+	self._translate_args = function(dictionary, googleargs, kwargs) {
 		// `dictionary` maps nicely named args (as keys) to
 		// google's API keys (as values). This takes in some
 		// kwartgs, and populates (mutates) googleargs
@@ -75,7 +76,7 @@ function GoogleReader() {
 		}
 	};
 
-	this.get_feed = function(opts, cb) {
+	self.get_feed = function(opts, cb) {
 		// returns a GoogleFeed, giving either an 'url' or a 'feed' internal name.
 		// other arguments may be any keys of GoogleReaderConst.ATOM_ARGS keys
 		var feedurl;
@@ -92,17 +93,16 @@ function GoogleReader() {
 		
 		urlargs = {};
 		opts['client'] = GoogleReaderConst.AGENT;
-		opts['timestamp'] = this.get_timestamp();
-		this._translate_args( GoogleReaderConst.ATOM_ARGS, urlargs, opts );
+		opts['timestamp'] = self.get_timestamp();
+		self._translate_args( GoogleReaderConst.ATOM_ARGS, urlargs, opts );
 
 		function inner_cb(data) {
 			cb(new Feed(data));
 		}
-		this.GET(feedurl, urlargs, inner_cb);
+		self.GET(feedurl, urlargs, inner_cb);
 	};
 
-	this.edit_api = function(edit_operation, arg_mapping, opts, cb) {
-		var self = this;
+	self.edit_api = function(edit_operation, arg_mapping, opts, cb) {
 		var urlargs = {};
 		urlargs['client'] = GoogleReaderConst.AGENT;
 
@@ -135,38 +135,38 @@ function GoogleReader() {
 
 	// medium-level
 
-	this.get_subscription_list = function() {
+	self.get_subscription_list = function() {
 		//get_subscription_list' returns a structure containing subscriptions.
-		this.get_api_list(GoogleReaderConst.URI_PREFIXE_API + GoogleReaderConst.API_LIST_SUBSCRIPTION, cb);
+		self.get_api_list(GoogleReaderConst.URI_PREFIXE_API + GoogleReaderConst.API_LIST_SUBSCRIPTION, cb);
 	}
 
-	this.get_tag_list = function(cb) {
-		this.get_api_list(GoogleReaderConst.URI_PREFIXE_API + GoogleReaderConst.API_LIST_TAG, {all:true}, cb);
+	self.get_tag_list = function(cb) {
+		self.get_api_list(GoogleReaderConst.URI_PREFIXE_API + GoogleReaderConst.API_LIST_TAG, {all:true}, cb);
 	};
 
 
-	this.get_unread_count_list = function(cb) {
+	self.get_unread_count_list = function(cb) {
 		// returns a structure containing the number of unread items in each subscriptions/tags.
-		this.get_api_list(GoogleReaderConst.URI_PREFIXE_API + GoogleReaderConst.API_LIST_UNREAD_COUNT, {all:true}, cb);
+		self.get_api_list(GoogleReaderConst.URI_PREFIXE_API + GoogleReaderConst.API_LIST_UNREAD_COUNT, {all:true}, cb);
 	};
 
-	this.edit_tag = function(opts, cb) {
+	self.edit_tag = function(opts, cb) {
 		opts = opts || {}
 		if(!('feed' in opts)) {
 			opts['feed'] = GoogleReaderConst.ATOM_STATE_READING_LIST;
 		}
 		opts['action'] = 'edit-tags';
-		this.edit_api(GoogleReaderConst.API_EDIT_TAG, GoogleReaderConst.EDIT_TAG_ARGS, opts, cb);
+		self.edit_api(GoogleReaderConst.API_EDIT_TAG, GoogleReaderConst.EDIT_TAG_ARGS, opts, cb);
 	};
 
 
 	// HIGH-level
 
-	this.get_all = function(cb) {
-		this.get_feed({}, cb);
+	self.get_all = function(cb) {
+		self.get_feed({}, cb);
 	};
 
-	this.get_tag_feed = function(tag, cb, opts) {
+	self.get_tag_feed = function(tag, cb, opts) {
 		if (opts == null) {
 			opts = {}
 		}
@@ -178,11 +178,10 @@ function GoogleReader() {
 
 		opts['feed'] = GoogleReaderConst.ATOM_PREFIXE_LABEL + tag;
 
-		return this.get_feed(opts, cb)
+		return self.get_feed(opts, cb)
 	};
 
-	this.get_user_tags = function(cb) {
-		var self = this;
+	self.get_user_tags = function(cb) {
 		self.get_tag_list(function(tag_list) {
 			var tags = tag_list.tags;
 			var tag_names = Array();
@@ -201,36 +200,36 @@ function GoogleReader() {
 		});
 	};
 
-	this.get_unread = function(cb) {
-		this.get_feed({exclude_target:GoogleReaderConst.ATOM_STATE_READ}, cb);
+	self.get_unread = function(cb) {
+		self.get_feed({exclude_target:GoogleReaderConst.ATOM_STATE_READ}, cb);
 	};
 
 
-	this.set_flag = function(entry, flag, add_or_remove, cb) {
+	self.set_flag = function(entry, flag, add_or_remove, cb) {
 		var args = {entry:entry};
 		var key = add_or_remove ? 'add' : 'remove';
 		args[key] = flag;
-		this.edit_tag(args, cb);
+		self.edit_tag(args, cb);
 	};
 
-	this.set_read = function(entry, val, cb) {
-		this.set_flag(entry, GoogleReaderConst.ATOM_STATE_READ, val, cb);
+	self.set_read = function(entry, val, cb) {
+		self.set_flag(entry, GoogleReaderConst.ATOM_STATE_READ, val, cb);
 	};
 
-	this.set_star = function(entry, val, cb) {
-		this.set_flag(entry, GoogleReaderConst.ATOM_STATE_STARRED, val, cb);
+	self.set_star = function(entry, val, cb) {
+		self.set_flag(entry, GoogleReaderConst.ATOM_STATE_STARRED, val, cb);
 	};
 
-	this.set_public = function(entry, val, cb) {
-		this.set_flag(entry, GoogleReaderConst.ATOM_STATE_BROADCAST, val, cb);
+	self.set_public = function(entry, val, cb) {
+		self.set_flag(entry, GoogleReaderConst.ATOM_STATE_BROADCAST, val, cb);
 	};
 
-	this.add_label = function(entry, labelname, cb) {
-		this.edit_tag({entry:entry, add:GoogleReaderConst.ATOM_PREFIXE_LABEL+labelname}, cb);
+	self.add_label = function(entry, labelname, cb) {
+		self.edit_tag({entry:entry, add:GoogleReaderConst.ATOM_PREFIXE_LABEL+labelname}, cb);
 	};
 
-	this.del_label = function(entry,labelname, cb) {
-		this.edit_tag({entry:entry, remove:GoogleReaderConst.ATOM_PREFIXE_LABEL+labelname}, cb);
+	self.del_label = function(entry,labelname, cb) {
+		self.edit_tag({entry:entry, remove:GoogleReaderConst.ATOM_PREFIXE_LABEL+labelname}, cb);
 	};
 
 }

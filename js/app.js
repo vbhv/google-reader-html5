f238e6a5c122a7de2e244cbff446cc801c7b56b5
tim <tim3d.junk@gmail.com>
2010-02-02 21:29:43 +1100

    moved things around

---
+++ js/app.js
@@ -0,0 +1,53 @@
+var LOGIN_DETAILS = {};
+
+function App(reader, store, sync, ui) {
+	this.reader = reader;
+	this.store = store;
+	this.ui = ui;
+	this.sync = sync
+	this.LOGGED_IN = false;
+	var self=this;
+
+	self.main = function() {
+		if (yield self.store.isEmpty.result()) {
+			console.log("starting a sync");
+			yield self.do_sync.result(true);
+		} else {
+			console.log("no sync needed");
+			var success = false;
+			self.ui.refresh(function() { success = true; });
+			window.setTimeout(function() {
+				if(!success) {
+					console.log("UI did not load after 5 seconds - forcing a fresh sync");
+					self.store.clear();
+					self.do_sync(true);
+				}
+			}, 5 * 1000);
+		}
+	};
+
+	self.do_sync = function(do_download, cb) {
+		yield self.ensure_login.result();
+		console.log("log")
+		if(do_download) {
+			console.log("run!")
+			yield self.sync.run.result();
+		} else {
+			console.log("push!")
+			yield self.sync.push.result();
+		}
+		console.log("now for a refresh")
+		yield self.ui.refresh.result();
+		cb();
+	};
+
+	self.ensure_login = function(cb){
+		if(self.LOGGED_IN) { cb(); }
+		yield self.reader.login.result(
+			LOGIN_DETAILS.user || prompt('User'),
+			LOGIN_DETAILS.password || prompt("Password"));
+		self.LOGGED_IN = true;
+		cb();
+	};
+}
+
--- js/main.js
+++ js/main.js
@@ -1,68 +1,13 @@
-var reader;
-var ui;
-var sync;
-var store;
-var LOGIN_DETAILS = {};
-var LOGGED_IN = false;
-
-function ensure_login(cb){
-	if(LOGGED_IN) { cb(); }
-	yield reader.login.result(
-		LOGIN_DETAILS.user || prompt('User'),
-		LOGIN_DETAILS.password || prompt("Password"));
-	LOGGED_IN = true;
-	cb();
-};
-
-function do_sync(do_download, cb) {
-	yield ensure_login.result();
-	console.log("log")
-	if(do_download) {
-		console.log("run!")
-		yield sync.run.result();
-	} else {
-		console.log("push!")
-		yield sync.push.result();
-	}
-	console.log("now for a refresh")
-	yield ui.refresh.result();
-	cb();
-}
-
-function add_an_underscore(item, cb) {
-	cb(item + "_");
-}
-
-function main2() {
-	var arr = ['a','b','c'];
-	var mapped = yield map_cb.result(arr, function(item, cb) {
-		cb(yield add_an_underscore.result(item));
-	});
-	alert(mapped);
-}
+var app;
 
 function main() {
-
-	reader = new GoogleReader();
-	store = new Store('dom');
+	var reader = new GoogleReader();
+	var store = new Store('dom');
 	// store = new Store();
-	sync = new Sync(reader, store);
-	ui = new UI(store);
-	if (yield store.isEmpty.result()) {
-		console.log("starting a sync");
-		yield do_sync.result(true);
-	} else {
-		console.log("no sync needed");
-		var success = false;
-		ui.refresh(function() { success = true; });
-		window.setTimeout(function() {
-			if(!success) {
-				console.log("UI did not load after 5 seconds - forcing a fresh sync");
-				store.clear();
-				do_sync(true);
-			}
-		}, 5 * 1000);
-	}
+	var sync = new Sync(reader, store);
+	var ui = new UI(store);
+	app = new App(reader, store, sync, ui);
+	app.main();
 };
 
 $(function() { async(main)()});

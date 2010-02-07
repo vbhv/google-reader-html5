45e101d6c1281a55a0778baa459fbe9845120916
tim <tim3d.junk@gmail.com>
2010-02-07 21:11:31 +1100

    added a process module that doesnt really work yet...

---
+++ js/process.js
@@ -0,0 +1,47 @@
+function Process() {
+	var self = this;
+
+	this.run(entry, cb) {
+		var body = jQuery(entry.body);
+		yield this.extract_image_urls.result(body);
+		body = yield this.add_alt_text.result(body);
+		yield this.insert_media.result(entry, body);
+		entry.body = body.toString();
+		cb();
+	};
+
+	function _subtitle(img) {
+		return jQuery("<em>" + img.attr('src') + "</em>");
+	}
+	
+	this.add_alt_text = function(content, cb) {
+		content = content.map(content, function(elm) {
+			elm = jQuery(elm);
+			if(elm.type == 'img') {
+				return [elm, _subtitle(elm)];
+			} else {
+				return elm;
+			}
+		}
+		content.each(function() {
+			jQuery(this).each(function() { this.after(_subtitle(this)); });
+		}
+		cb(content);
+	};
+
+	this.insert_media = function(entry, body, cb) {
+		// TODO: extract enclosure URLs
+	};
+
+	this.extract_image_urls = function(content, cb) {
+		var top_level_images = content.filter('img');
+		content.each(function() {
+			top_level_images += this.find('img');
+		}
+		return jQuery.map(top_level_images, function(img) {
+			return jQuery(img).attr(src);
+		}
+	};
+
+}
+
